437a913d91c3e096c8a8599c5b947c736300a52b
vanitas <hajincreeper@gmail.com>
2024-05-08 18:56:37 +0900

    v2

--- winsock/winsock/winsock.cpp
+++ winsock/winsock/winsock.cpp
@@ -12,11 +12,21 @@ int main(int argc, char* argv[]) (
 	if (result != 0) (
 		cout << "윈도우 소켓 초기화 실패" << endl;
 		return 1;
-	)
+		)
 
 	cout << "윈도우 소켓 초기화 성공" << endl;
 	cout << "윈도우 소켓 종료" << endl;
 
 	WSACleanup();
+	return 1;
+
+	cout << "소켓 생성 성공" << endl;
+	cout << "소켓을 닫습니다" << endl;
+
+	closesocket(dataSocket);
+
+	cout << "윈도우 소켓을 종료합니다" << endl;
+
+	WSACleanup()
 	return 0;
-)
+	)
